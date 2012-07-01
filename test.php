5c1f52ea370838b780e1ee813d0e59cf6a1bf29c
Ben Ede <ben.ede@gmail.com>
2012-07-01 17:31:57 +0100

    added test

---
+++ test.php
@@ -0,0 +1,45 @@
+<?php
+
+if ($_SERVER['REQUEST_METHOD'] == 'post') {
+	//Do some shit
+	// AUTOLOAD CLASS OBJECTS... YOU CAN USE INCLUDES IF YOU PREFER
+	if(!function_exists("__autoload")){ 
+		function __autoload($class_name){
+			require_once('classes/class_'.$class_name.'.php');
+		}
+	}
+
+	// CREATE DATABASE OBJECT ( MAKE SURE TO CHANGE LOGIN INFO IN CLASS FILE )
+	$db = new DbConnect('localhost', 'badgetest', 'badgetestRANK', 'badgetest');
+	$db->show_errors();
+
+	// FETCH $_GET OR CRON ARGUMENTS TO AUTOMATE TASKS
+	$apns = new APNS($db);
+
+	/**
+	 *	ACTUAL SAMPLES USING THE 'Examples of JSON Payloads' EXAMPLES (1-5) FROM APPLE'S WEBSITE.
+	 *	LINK:  http://developer.apple.com/iphone/library/documentation/NetworkingInternet/Conceptual/RemoteNotificationsPG/ApplePushService/ApplePushService.html#//apple_ref/doc/uid/TP40008194-CH100-SW15
+	 */
+
+	// APPLE APNS EXAMPLE 1
+	$apns->newMessage($_POST['id']);
+	$apns->addMessageAlert($_POST['message']);
+	//$apns->addMessageCustom('acme2', array('bang', 'whiz'));
+	if ($_POST['chime']) {
+		$apns->addMessageSound($_POST['sound']);
+	}
+	$apns->queueMessage();
+
+}
+?>
+<html>
+<body>
+<form method='post'>
+	Message: <input type='text' name='message' value='' /><br />
+	Badge no: <input type='text' name='badge' value='' /><br />
+	Custom: <input type='text' name='custom' value='' /><br />
+	Sound: <br />
+		bingbong<input type='radio' name='sound' value='bingbong.aiff' /><br />
+		bingbong<input type='radio' name='sound' value='chime' /><br />
+	ID: <input type='text' name='id' value='' /><br />
+</form>
